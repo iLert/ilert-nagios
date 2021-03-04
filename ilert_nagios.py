699a1c73aa28978556f419e8ac88b7e4bf324b1c
yacut <roman.rogozhnikov@gmail.com>
2021-03-04 11:35:39 +0100

    update copyright

--- ilert_nagios.py
+++ ilert_nagios.py
@@ -3,7 +3,7 @@
 
 # iLert Nagios/Icinga/Check_MK Plugin
 #
-# Copyright (c) 2013-2020, iLert GmbH. <support@ilert.com>
+# Copyright (c) 2013-2021, iLert GmbH. <support@ilert.com>
 # All rights reserved.
 
 
